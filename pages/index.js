1a237af25efc198e7e80d39fead7edb67919fffe
Gustavo Paulino <63270867+gustavopln@users.noreply.github.com>
2024-11-28 03:07:53 +0000

    inclusão imagem família

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
+import Image from "next/image";
+
 function Home() {
-    return <h1>Eu amo minha família!!! ❤️❤️❤️ </h1>
+    return (
+        <>
+            <h1>Eu amo minha família!!! ❤️❤️❤️ </h1>
+            <Image
+                src="/images/hot_park.jpg"
+                alt="Família no Hot Park"
+                width={544}
+                height={327}
+            />    
+            
+        </>
+    )
 }
 
 export default Home;
